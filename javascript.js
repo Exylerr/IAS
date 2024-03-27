a2669f81c6ab4bd0c8e85c8bc1c2e88fa882165b
Exylerr <131851485+Exylerr@users.noreply.github.com>
2024-03-27 12:44:10 +0800

    commit

--- javascript.js
+++ javascript.js
@@ -1,11 +1,21 @@
 document.getElementById('noautho').addEventListener('click', function() {
     var fileUrl = 'PUP Website.pdf';
-    var a = document.createElement('a');
-    a.href = fileUrl;
-    a.download = 'PUP Website.pdf';
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
+            
+    fetch(fileUrl)
+    .then(response => response.blob())
+    .then(blob => {
+        var a = document.createElement('a');
+        var url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = 'PUP Website.pdf';
+        document.body.appendChild(a);
+        a.click();
+        window.URL.revokeObjectURL(url);
+        document.body.removeChild(a);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 });
 
 document.getElementById('withautho').addEventListener('click', function() {
